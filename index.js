cd0d203c7232043d39dfdf0bc8c36dfc36ce2b48
CodeBrew28 <arshiam2@illinois.edu>
2018-02-24 03:19:40 -0600

    line count

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 #!/usr/bin/env node
 
-const argv = require('yargs').usage('Usage: $0 --ships [num] --distance [num]').argv
- 
-if (argv.ships > 3 && argv.distance < 53.5) {
-  console.log('Plunder more riffiwobbles!')
-} else {
-  console.log('Retreat from the xupptumblers!')
-}
+var argv = require('yargs')
+    .usage('Usage: $0 <command> [options]')
+    .command('count', 'Count the lines in a file')
+    .example('$0 count -f foo.js', 'count the lines in the given file')
+    .alias('f', 'file')
+    .nargs('f', 1)
+    .describe('f', 'Load a file')
+    .demandOption(['f'])
+    .help('h')
+    .alias('h', 'help')
+    .epilog('copyright 2015')
+    .argv;
+
+var fs = require('fs');
+var s = fs.createReadStream(argv.file);
+
+var lines = 0;
+s.on('data', function (buf) {
+    lines += buf.toString().match(/\n/g).length 
+});
+
+s.on('end', function () {
+    console.log(lines);
+});
