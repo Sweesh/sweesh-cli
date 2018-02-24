189c6ccb668eebcc72ae17b71771ed746bb252bb
Brian Kurek <brkurek@gmail.com>
2018-02-24 15:52:22 -0600

    Use commandDir instead of command

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import resolve from './resolve'; // eslint-disable-line no-unused-vars
 
 // not sure if we can use ES6 imports for yargs, so use require for now
 const argv = require('yargs') // eslint-disable-line no-unused-vars
-    .command(require('./commands/login'))
+    .commandDir('./commands')
     .help()
     .argv;
