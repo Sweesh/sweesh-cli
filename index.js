cb285727422a12c44236180d2f3e7882453918f3
CodeBrew28 <arshiam2@illinois.edu>
2018-02-24 03:12:16 -0600

    usage

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const argv = require('yargs').argv
+const argv = require('yargs').usage('Usage: $0 --ships [num] --distance [num]').argv
  
 if (argv.ships > 3 && argv.distance < 53.5) {
   console.log('Plunder more riffiwobbles!')
