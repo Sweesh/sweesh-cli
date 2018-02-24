01bc3ff677c6f09dc0bc970966cb6b851ca25f89
Pranay Prakash <pranay.gp@gmail.com>
2018-02-24 13:55:58 -0600

    resolve module v1

--- src/resolve.js
+++ src/resolve.js
@@ -1,10 +1,21 @@
+import { isNull } from "util";
+
 /**
  * The resolve module will call all the provided resolvers, interface with stdin to deal with ambiguities, and finally ALWAYS resolve a 'CHANGE' intent.
  * If we can't, this module will throw an Error.
  */
 
-const resolve = (resolvers) => {
-  return {
-    path: "./test.config"
+const resolve = async (resolvers, path) => {
+  let resolved = await Promise.all(resolvers.map(({fn}) => fn({path})));
+  resolved = resolved.map(Boolean);
+  if(!resolved.length) {
+    throw Error(`Unable to resolve ${path}`)
+  }
+  if(resolved.length > 1){
+    // TODO: disambiguate from the CLI
+  } else {
+    return resolved[0];
   }
 }
+
+module.exports = resolve;
