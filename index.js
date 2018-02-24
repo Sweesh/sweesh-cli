6832110788b1a5a59e51575ac0c517d1ade320e9
CodeBrew28 <arshiam2@illinois.edu>
2018-02-24 03:30:41 -0600

    prints a file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-
-var argv = require('yargs')
+const fs = require('fs');
+const argv = require('yargs')
     .usage('Usage: $0 <command> [options]')
     .command('add', 'Adds a file')
     .example('$0 add -f foo.js', 'puts a file on the database')
@@ -11,16 +11,12 @@ var argv = require('yargs')
     .help('h')
     .alias('h', 'help')
     .argv;
-
-var fs = require('fs');
-var s = fs.createReadStream(argv.file);
+const s = fs.createReadStream(argv.file);
 
 var lines = "";
 s.on('data', function (buf) {
     lines += buf.toString()
 });
-
-
 
 s.on('end', function () {
     console.log(lines);
