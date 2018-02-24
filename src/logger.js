cdccf713fcd0f1719426cf066190e7ff1b5ab2e5
CodeBrew28 <arshiam2@illinois.edu>
2018-02-24 16:25:13 -0600

    get command and purge for logger

--- src/logger.js
+++ src/logger.js
@@ -1,18 +1,28 @@
 import fs from 'fs';
-
-
 class Logger {
     constructor() {
         this.file = 'log.json'
-        this.data = { "users": [] }
+        this.data = { "commands": [] }
         fs.writeFileSync(this.file, JSON.stringify(this.data))
     }
   
     addCommand(command, old_path, new_path) {
-        (this.data["users"]).push({"command": command, "old_path": old_path, "new_path": new_path}) 
+        (this.data["commands"]).push({"command": command, "old_path": old_path, "new_path": new_path}) 
         fs.writeFileSync(this.file, JSON.stringify(this.data))
+    }
+
+    getCommands() {
+        return JSON.stringify(this.data)
+    }
+
+    purge() {
+        this.data = { "commands": [] }
+        fs.writeFileSync(this.file, JSON.stringify(this.data))  
     }
 }
 
-const logger = new Logger()
-logger.addCommand("add test.txt", "test.txt","aria")
+// const logger = new Logger()
+// logger.addCommand("add test.txt", "test.txt","aria")
+// console.log(logger.getCommands())
+// logger.purge()
+// console.log(logger.getCommands())
