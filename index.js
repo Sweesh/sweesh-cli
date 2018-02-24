411eeceb51cf25dc00c2ec55c47a5cf1f51e387c
CodeBrew28 <arshiam2@illinois.edu>
2018-02-24 03:29:15 -0600

    adds a file

--- index.js
+++ index.js
@@ -2,24 +2,25 @@
 
 var argv = require('yargs')
     .usage('Usage: $0 <command> [options]')
-    .command('count', 'Count the lines in a file')
-    .example('$0 count -f foo.js', 'count the lines in the given file')
+    .command('add', 'Adds a file')
+    .example('$0 add -f foo.js', 'puts a file on the database')
     .alias('f', 'file')
     .nargs('f', 1)
     .describe('f', 'Load a file')
     .demandOption(['f'])
     .help('h')
     .alias('h', 'help')
-    .epilog('copyright 2015')
     .argv;
 
 var fs = require('fs');
 var s = fs.createReadStream(argv.file);
 
-var lines = 0;
+var lines = "";
 s.on('data', function (buf) {
-    lines += buf.toString().match(/\n/g).length 
+    lines += buf.toString()
 });
+
+
 
 s.on('end', function () {
     console.log(lines);
