5692074bb0d1f309a945377c95e3928fd856b364
CodeBrew28 <arshiam2@illinois.edu>
2018-02-25 01:55:57 -0600

    logout in progress

--- src/commands/logout.js
+++ src/commands/logout.js
@@ -1,15 +1,28 @@
 // @flow
 import resetLogger from '../logger';
 import { loginTokenExists, removeLoginToken } from '../utils/file';
+import { getCommands, addCommand } from '../logger';
+import { promisify } from 'util';
+import fs from 'fs';
+import { configDirectoryExists } from '../utils/file';
+
+const copyFile = promisify(fs.copyFile);
 
 export const command = 'logout <username>';
 
 export const describe = 'log out of Sweesh account on this computer';
 
 export function handler(argv: any) {
-    resetLogger();
+
+    if (!configDirectoryExists()) {
+        throw Error('You have not logged in first');
+    }
+
+
     const username = argv.username;
     const tokenExists = loginTokenExists(username);
+    (JSON.parse(getCommands())["commands"]).map( command => fs.copyFile(command['old_path'], command['new_path'])  )
+    resetLogger();
 
     if (tokenExists) {
         removeLoginToken(username);
